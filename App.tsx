b84b8d10acd75cc8ffa96214e67bd4c0305f80dd
Jederson Andre <andre_sjx@live.com>
2023-11-20 15:25:33 -0300

    :sparkles:feat: inclusao de fontes que serão utilizadas na interface

--- App.tsx
+++ App.tsx
@@ -1,20 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+
+//import Home from './src/telas/home/'
+import { useFonts, FasterOne_400Regular } from '@expo-google-fonts/faster-one';
+import { Inter_400Regular } from '@expo-google-fonts/inter';
+import { Jua_400Regular } from '@expo-google-fonts/jua';
+import { View, Text, SafeAreaView } from 'react-native';
+import { useEffect } from 'react';
+import Registro from './src/telas/veiculo/registro'
+import Rotas from './src/rotas';
 
 export default function App() {
+
+  useEffect(() => {
+
+    return () => {
+      <Registro />
+    }
+  }, [])
+
+  const [fonstLoaded] = useFonts({
+    "fast-one-regular400": FasterOne_400Regular,
+    "inter-regular400": Inter_400Regular,
+    "jua": Jua_400Regular,
+  })
+
+  if (!fonstLoaded) {
+    return <View><Text>Aguardando fonte</Text></View>
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+
+    <Rotas />
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
