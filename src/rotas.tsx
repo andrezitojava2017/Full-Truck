f7871ff8e6408760425606d76cb8b178891e6045
Jederson Andre <andre_sjx@live.com>
2023-11-23 22:03:36 -0300

    :sparkles:feat: ocultando o cabeçalho das rotas

--- src/rotas.tsx
+++ src/rotas.tsx
@@ -12,10 +12,16 @@ const Rotas = () => {
         <NavigationContainer>
             <Tab.Navigator screenOptions={{ tabBarHideOnKeyboard: true }}>
                 <Tab.Screen name="Home" component={Home} />
-                <Tab.Screen name="Registro" component={Registro} options={
-                    { headerShown: false }
-                } />
-                <Tab.Screen name="Abastecimento" component={Abastecimento} />
+                <Tab.Screen name="Registro"
+                    component={Registro}
+                    options={
+                        { headerShown: false }
+                    } />
+                <Tab.Screen name="Abastecimento"
+                    component={Abastecimento}
+                    options={
+                        { headerShown: false }
+                    } />
             </Tab.Navigator>
         </NavigationContainer>
     )
