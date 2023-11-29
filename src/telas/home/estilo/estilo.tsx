b9154b678f37dabad24f23040649f208d0424d59
Jederson Andre <andre_sjx@live.com>
2023-11-28 21:12:02 -0300

    :sparkles: feat: implementado a screen Home e estilo da interface

---
+++ src/telas/home/estilo/estilo.tsx
@@ -0,0 +1,19 @@
+import { StyleSheet } from 'react-native'
+
+const estilo = StyleSheet.create({
+
+    container: {
+        flex: 1,
+        backgroundColor: '#036564',
+        padding: 14,
+    },
+    referencia: {
+        flexDirection: 'row',
+        backgroundColor: "#FFFEFE",
+        justifyContent: 'space-around',
+        padding: 10,
+
+    }
+})
+
+export default estilo;
--- src/telas/home/home.tsx
+++ src/telas/home/home.tsx
@@ -1,9 +1,18 @@
 import { View, Text } from "react-native"
+import estilo from './estilo/estilo'
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import Card from "../../components/card/card";
 
 const Home = () => {
     return (
-        <View>
-            <Text>Home screen</Text>
+        <View style={estilo.container}>
+
+            <View style={estilo.referencia}>
+                <Text style={{ fontFamily: "inter-regular400", fontSize: 20, color: "#443E45" }}>Referencia</Text>
+                <Text style={{ fontFamily: "inter-regular400", fontSize: 20, color: "#443E45" }}>Dezembro</Text>
+                <MaterialCommunityIcons name={"calendar-search"} size={22} color="#443E45" />
+            </View>
+            <Card />
         </View>
     )
 }
