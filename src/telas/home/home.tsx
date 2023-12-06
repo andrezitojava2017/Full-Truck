e9358a9bb2f2c61fa519618d9872a708de10099a
Jederson Andre <andre_sjx@live.com>
2023-12-05 22:34:52 -0300

    :sparkles: feat: implementação de logica para carregamento e exibição de lista de abastecimentos registradas na base de dados

--- src/telas/home/home.tsx
+++ src/telas/home/home.tsx
@@ -1,9 +1,35 @@
-import { View, Text } from "react-native"
+import { View, Text, FlatList } from "react-native"
 import estilo from './estilo/estilo'
 import { MaterialCommunityIcons } from '@expo/vector-icons';
 import Card from "../../components/card/card";
+import { useEffect, useState } from "react";
+import { listaAbastecimento } from "../../serivces/abastecimento";
+import Abastecimento from "../abastecimento/abastecimento";
 
 const Home = () => {
+
+    type Abastecimento = {
+        id: string;
+        litros: string;
+        preco: string;
+        combustivel: string;
+        kilometragem: string;
+        data: string;
+    }
+
+    const [listAbastecimento, setListAbastecimento] = useState<Abastecimento[]>([{ id: '0', litros: '0', preco: "", combustivel: "", kilometragem: "", data: "" }])
+
+    useEffect(() => {
+        (async () => {
+            const lista = await listaAbastecimento();
+            if (lista) {
+                setListAbastecimento([...lista]);
+
+                console.log('dados abastecimento:\n', lista)
+            }
+        })();
+    }, [])
+
     return (
         <View style={estilo.container}>
 
@@ -12,7 +38,13 @@ const Home = () => {
                 <Text style={{ fontFamily: "inter-regular400", fontSize: 20, color: "#443E45" }}>Dezembro</Text>
                 <MaterialCommunityIcons name={"calendar-search"} size={22} color="#443E45" />
             </View>
-            <Card />
+
+            <FlatList
+                data={listAbastecimento}
+                renderItem={({ item }) => (<Card values={item} />)}
+                keyExtractor={(item) => (item.id)}
+            />
+
         </View>
     )
 }
