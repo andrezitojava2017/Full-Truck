d19e5209483fdef1c533d13ee24347e8eade167d
Jederson Andre <andre_sjx@live.com>
2023-12-09 22:12:46 -0300

    :sparkles: feat: implementação de logica para buscar novos registro de abastecimento com refresh do flatList e exibição dos dados

--- src/telas/home/home.tsx
+++ src/telas/home/home.tsx
@@ -3,7 +3,7 @@ import estilo from './estilo/estilo'
 import { MaterialCommunityIcons } from '@expo/vector-icons';
 import Card from "../../components/card/card";
 import { useEffect, useState } from "react";
-import { listaAbastecimento } from "../../serivces/abastecimento";
+import { listaAbastecimento, maiorValorKm } from "../../serivces/abastecimento";
 import Abastecimento from "../abastecimento/abastecimento";
 
 const Home = () => {
@@ -15,20 +15,34 @@ const Home = () => {
         combustivel: string;
         kilometragem: string;
         data: string;
+        media: string;
     }
 
-    const [listAbastecimento, setListAbastecimento] = useState<Abastecimento[]>([{ id: '0', litros: '0', preco: "", combustivel: "", kilometragem: "", data: "" }])
+    const [listAbastecimento, setListAbastecimento] = useState<Abastecimento[]>([{ id: '0', litros: '0', preco: "", combustivel: "", kilometragem: "", data: "", media: "" }])
+    const [exibirRefresh, setExibirRefresh] = useState<boolean>(false)
 
     useEffect(() => {
         (async () => {
             const lista = await listaAbastecimento();
+            await maiorValorKm();
+
             if (lista) {
                 setListAbastecimento([...lista]);
-
-                console.log('dados abastecimento:\n', lista)
             }
         })();
     }, [])
+
+
+    const atualizarLista = async () => {
+
+        setExibirRefresh(true);
+        const lista = await listaAbastecimento();
+
+        if (lista) {
+            setListAbastecimento([...lista]);
+            setExibirRefresh(false)
+        }
+    }
 
     return (
         <View style={estilo.container}>
@@ -43,6 +57,8 @@ const Home = () => {
                 data={listAbastecimento}
                 renderItem={({ item }) => (<Card values={item} />)}
                 keyExtractor={(item) => (item.id)}
+                refreshing={exibirRefresh}
+                onRefresh={atualizarLista}
             />
 
         </View>
