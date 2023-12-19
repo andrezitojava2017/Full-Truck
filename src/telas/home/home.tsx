cc1233faea98ee41e0bb7e34ca79f6558cf50ddf
Jederson Andre <andre_sjx@live.com>
2023-12-18 21:35:13 -0300

    :sparkles: feat: implementado logica e funções necessárias para exibir o componente DatePicker para usuario selecionar um mês para referencia

--- src/telas/home/home.tsx
+++ src/telas/home/home.tsx
@@ -1,10 +1,13 @@
-import { View, Text, FlatList } from "react-native"
+import { View, Text, FlatList, TouchableOpacity } from "react-native"
 import estilo from './estilo/estilo'
 import { MaterialCommunityIcons } from '@expo/vector-icons';
 import Card from "../../components/card/card";
 import { useEffect, useState } from "react";
 import { listaAbastecimento, maiorValorKm } from "../../serivces/abastecimento";
 import Abastecimento from "../abastecimento/abastecimento";
+import DateTimePicker, { DateTimePickerAndroid, DateTimePickerEvent } from '@react-native-community/datetimepicker';
+import Mensagem from "../../components/mensagem/mensagem";
+
 
 const Home = () => {
 
@@ -18,8 +21,10 @@ const Home = () => {
         media: string;
     }
 
+    const meses = ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro']
     const [listAbastecimento, setListAbastecimento] = useState<Abastecimento[]>([{ id: '0', litros: '0', preco: "", combustivel: "", kilometragem: "", data: "", media: "" }])
     const [exibirRefresh, setExibirRefresh] = useState<boolean>(false)
+    const [mesReferencia, setMesReferencia] = useState<string>("")
 
     useEffect(() => {
         (async () => {
@@ -31,6 +36,28 @@ const Home = () => {
             }
         })();
     }, [])
+
+    const recuperarMesSelecionado = (event: DateTimePickerEvent, dataSelecionada: Date | any) => {
+
+
+        if (dataSelecionada === undefined || event.type === 'dismissed') {
+            const mesAtual = new Date().getMonth();
+            //console.log('Cancelar clicado: ' + meses[mesAtual])
+            setMesReferencia(meses[mesAtual])
+        }
+
+        const refSelecionada = meses[dataSelecionada.getMonth()];
+        setMesReferencia(refSelecionada);
+    }
+
+    const exibirDatePicker = () => {
+        DateTimePickerAndroid.open({
+            mode: 'date',
+            value: new Date(),
+            onChange: (event, selectedDate) => recuperarMesSelecionado(event, selectedDate),
+
+        })
+    }
 
 
     const atualizarLista = async () => {
@@ -48,18 +75,32 @@ const Home = () => {
         <View style={estilo.container}>
 
             <View style={estilo.referencia}>
-                <Text style={{ fontFamily: "inter-regular400", fontSize: 20, color: "#443E45" }}>Referencia</Text>
-                <Text style={{ fontFamily: "inter-regular400", fontSize: 20, color: "#443E45" }}>Dezembro</Text>
-                <MaterialCommunityIcons name={"calendar-search"} size={22} color="#443E45" />
+                <Text style={{ fontFamily: "inter-regular400", fontSize: 20, color: "#443E45" }}>Referencia:</Text>
+
+                <TouchableOpacity onPress={exibirDatePicker} style={{ flexDirection: 'row', alignItems: 'center' }}>
+
+                    {
+                        mesReferencia != "" ? <Text style={{ fontFamily: "inter-regular400", fontSize: 20, color: "#443E45" }}>{mesReferencia}</Text>
+                            : <Text style={{ fontFamily: "inter-regular400", fontSize: 20, color: "#443E45" }}>***</Text>
+                    }
+
+
+                    <MaterialCommunityIcons name={"calendar-search"} size={22} color="#443E45" style={{ paddingLeft: 14 }} />
+                </TouchableOpacity>
+
             </View>
 
-            <FlatList
-                data={listAbastecimento}
-                renderItem={({ item }) => (<Card values={item} />)}
-                keyExtractor={(item) => (item.id)}
-                refreshing={exibirRefresh}
-                onRefresh={atualizarLista}
-            />
+            {
+                mesReferencia === "" ? <Mensagem />
+                    : <FlatList
+                        data={listAbastecimento}
+                        renderItem={({ item }) => (<Card values={item} />)}
+                        keyExtractor={(item) => (item.id)}
+                        refreshing={exibirRefresh}
+                        onRefresh={atualizarLista}
+                    />
+            }
+
 
         </View>
     )
