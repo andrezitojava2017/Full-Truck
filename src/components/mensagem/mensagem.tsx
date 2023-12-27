5465d5aecb8102e67fffb5d4e109f00fb1df7cde
Jederson Andre <andre_sjx@live.com>
2023-12-27 10:58:04 -0300

    :recycle: refactor: personalização de novo recurso na exibição de mensagem

--- src/components/mensagem/mensagem.tsx
+++ src/components/mensagem/mensagem.tsx
@@ -1,9 +1,11 @@
 import { Text, View } from "react-native"
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 
-const Mensagem = () => {
+const Mensagem = (props: any) => {
     return (
         <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Text style={{ color: '#ffff', fontSize: 16 }}>Por favor, selecione um mês de referencia</Text>
+            <MaterialCommunityIcons name={"information-off"} size={50} color="#f07167" />
+            <Text style={{ color: '#ffff', fontSize: 16 }}>{props.message}</Text>
         </View>
     )
 
