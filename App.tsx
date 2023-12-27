fab41dd23c3a1ca45ef5320aff5ecc4cc0794734
Jederson Andre <andre_sjx@live.com>
2023-12-27 11:30:05 -0300

    :sparkles: feat: inclusao de função para buscar registro de veiculo cadastrado; implementado logica, caso veiculo nao esteja cadastrado, o usuario seja enviado para a tela de registro de veiculo

--- App.tsx
+++ App.tsx
@@ -1,22 +1,32 @@
 
-//import Home from './src/telas/home/'
+import { useState } from 'react';
 import { useFonts, FasterOne_400Regular } from '@expo-google-fonts/faster-one';
 import { Inter_400Regular } from '@expo-google-fonts/inter';
 import { Jua_400Regular } from '@expo-google-fonts/jua';
-import { View, Text, SafeAreaView } from 'react-native';
+import { View, Text } from 'react-native';
 import { useEffect } from 'react';
 import Registro from './src/telas/veiculo/registro'
 import Rotas from './src/rotas';
-import { criarTabelaRegistroDeVeiculo } from './src/serivces/tblVeiculo';
+import { buscarDadosVeiculo, criarTabelaRegistroDeVeiculo } from './src/serivces/tblVeiculo';
 import { criarTabelaAbastecimento } from './src/serivces/abastecimento';
 
 export default function App() {
 
+  const [veiculoRegistrado, setVeiculoRegistrado] = useState(false);
+  const buscarRegistroVeiculo = async () => {
+    buscarDadosVeiculo().then((rs) => {
+      if (rs.length !== 0) {
+        setVeiculoRegistrado(true)
+        console.log('Veiculo cadastrado: ', rs)
+      }
+
+    })
+  }
 
   useEffect(() => {
     criarTabelaRegistroDeVeiculo();
     criarTabelaAbastecimento();
-
+    buscarRegistroVeiculo();
   }, [])
 
   const [fonstLoaded] = useFonts({
@@ -29,9 +39,14 @@ export default function App() {
     return <View><Text>Aguardando fonte</Text></View>
   }
 
-  return (
+  if (veiculoRegistrado) {
+    return <Rotas />
 
-    <Rotas />
-  );
+  }
+
+  return (
+    <Registro />
+  )
+
 }
 
